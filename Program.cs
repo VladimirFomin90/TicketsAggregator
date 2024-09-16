4e90c7b892af4bb056975ac0763a9981387b0056
VladimirFomin90 <VladimirFomin90@yandex.ru>
2024-09-16 22:59:22 +0500

    parsing tickets, splitting a string into parts

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿const string TicketsFolder = @"/home/vladimir/RiderProjects/TicketsAggregator/src/Tickets";
+﻿using UglyToad.PdfPig;
+
+const string TicketsFolder = @"/home/vladimir/RiderProjects/TicketsAggregator/src/Tickets";
 
 try
 {
@@ -25,5 +27,22 @@ public class TicketsAggregator
 
     public void Run()
     {
+        foreach (var filepath in Directory.GetFiles(_ticketsFolder, "*.pdf"))
+        {
+            using var document = PdfDocument.Open(filepath);
+
+            // Page number starts from 1, not 0.
+            var page = document.GetPage(1);
+            var text = page.Text;
+            var splitString = text.Split(new[] { "Title:", "PMTitle:", "Date:", "Time:", "Visit us:" },
+                StringSplitOptions.None);
+
+            for (var i = 1; i < splitString.Length - 3; i += 3)
+            {
+                var title = splitString[i];
+                var date = splitString[i + 1];
+                var time = splitString[i + 2];
+            }
+        }
     }
 }
